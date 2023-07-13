d5cb464645d97039a4f3d25dc800857569dd00ef
Hemnani Mihir Jitumal <mihir.hemnani2336.gmail.com>
2023-07-13 23:45:12 +0530

    final

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-require_once "vendor/autoload.php";
-use Omnipay\Omnipay;
+// require_once "vendor/autoload.php";
+// use Omnipay\Omnipay;
 // define('CLIENT_ID', 'AUfd5be-9fODSbhkRTLP2zbRliAt69UEZ27lDBpLGluXnyWjuUUcS4VBDR4lkelXixhoi_Ege312mIXW');
 // define('CLIENT_SECRET', 'EEgh697RVsVwXNglx2IEt42pcZSr5gBksId9oAP6IIMt85C2qyh-efIfrbru_YFtUpDAWAy13aaPqDJZ');
 // define('PAYPAL_CURRENCY', 'USD');
 // define('PAYPAL_RETURN_URL', 'https://anydaytutors.com/success.php');
 // define('PAYPAL_CANCEL_URL', 'https://anydaytutors.com/cancel.php');
 try{
-        $gateway = Omnipay::create('PayPal_Rest');
+        // $gateway = Omnipay::create('PayPal_Rest');
         // $gateway->setClientId(CLIENT_ID);
         // $gateway->setSecret(CLIENT_SECRET);
         // $gateway->setTestMode(true);
--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer>
+  <br>
 <p>Copyright &copy; 2022 - AnyDay Tutors. All Rights Reserved. || Created by
       <a href="https://mihirhemnani.github.io/Portfolio/" target="_black">
-        <img class="logo-bottom" src="./wiz.png">Mihir 
+        Mihir 
       </a>
 </p> 
 </footer>
