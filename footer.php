d81b141c8f2b5dc4d436703e92584bff6ff894e2
Hemnani Mihir Jitumal <mihir.hemnani2336.gmail.com>
2023-07-13 23:26:45 +0530

    final

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
-<p>Copyright &copy; 2022 - AnyDay Tutors. All Rights Reserved. || Magically Created by
-      <a href=" https://www.thewizards.me" target="_blank">
-        <img class="logo-bottom" src="./wiz.png">The&nbsp;Wizards
+<p>Copyright &copy; 2022 - AnyDay Tutors. All Rights Reserved. || Created by
+      <a href="https://mihirhemnani.github.io/Portfolio/" target="_black">
+        <img class="logo-bottom" src="./wiz.png">Mihir 
       </a>
 </p> 
 </footer>
